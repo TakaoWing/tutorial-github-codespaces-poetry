a12021df269b619f3848f16896dc14d8c4c73468
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2025-02-01 14:54:06 +0000

    Remove unnecessary environment variables from Docker Compose and fix endpoint URL format in test script

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from azure.cosmos import CosmosClient, exceptions
 
 # Initialize the Cosmos client
-endpoint = "https://localhost:8081/"
+endpoint = "https://localhost:8081"
 key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 client = CosmosClient(endpoint, key)
 
